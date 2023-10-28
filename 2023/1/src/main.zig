9e8faa63019f0fea6f63afbbfee501ec496793f8
Silas Marvin <19626586+SilasMarvin@users.noreply.github.com>
2023-10-28 13:24:52 -0700

    Solved day 1 part 2

--- 2023/1/src/main.zig
+++ 2023/1/src/main.zig
@@ -10,15 +10,25 @@ pub fn main() !void {
     // We know the size of this is 10518
     _ = try f.readAll(&input);
 
-    var max: usize = 0;
+    var elf1_calorie_count: usize = 0;
+    var elf2_calorie_count: usize = 0;
+    var elf3_calorie_count: usize = 0;
+
     var current_sum: usize = 0;
     var last_byte: u8 = 0;
     var current_num: usize = 0;
     for (input) |byte| {
         if (byte == '\n') {
             if (last_byte == '\n') {
-                if (current_sum > max) {
-                    max = current_sum;
+                if (current_sum > elf1_calorie_count) {
+                    elf3_calorie_count = elf2_calorie_count;
+                    elf2_calorie_count = elf1_calorie_count;
+                    elf1_calorie_count = current_sum;
+                } else if (current_sum > elf2_calorie_count) {
+                    elf3_calorie_count = elf2_calorie_count;
+                    elf2_calorie_count = current_sum;
+                } else if (current_sum > elf3_calorie_count) {
+                    elf3_calorie_count = current_sum;
                 }
                 current_sum = 0;
             }
@@ -31,5 +41,6 @@ pub fn main() !void {
         last_byte = byte;
     }
 
-    std.debug.print("Answer: {}", .{max});
+    std.debug.print("Part 1 Answer: {}\n", .{elf1_calorie_count});
+    std.debug.print("Part 2 Answer: {}", .{elf1_calorie_count + elf2_calorie_count + elf3_calorie_count});
 }
